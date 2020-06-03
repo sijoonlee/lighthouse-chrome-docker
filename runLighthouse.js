415a01c8b9b8b562a8650f1a5aef5cc38bba24bf
Sijoon Lee <shijoonlee@gmail.com>
2020-06-03 15:18:27 -0700

    first draft

---
+++ runLighthouse.js
@@ -0,0 +1,49 @@
+const {writeFile} = require('fs');
+const {promisify} = require('util');
+const pWriteFile = promisify(writeFile);
+const chromeLauncher = require('chrome-launcher');
+const lighthouse = require('lighthouse');
+const {URL} = require('url');
+
+function launchChromeAndRunLighthouse(url, opts, config = null) {
+  return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
+    opts.port = chrome.port;
+    return lighthouse(url, opts, config).then(results => {
+      // use results.lhr for the JS-consumable output
+      // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
+      // use results.report for the HTML/JSON/CSV output as a string
+      // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
+      return chrome.kill().then(() => results)
+    });
+  });
+}
+
+const run = async (url = 'https://ratehub.ca') => {
+
+  const opts = {
+    chromeFlags: [
+      '--disable-dev-shm-usage',
+      '--headless',
+      '--no-sandbox',
+      '--ignore-certificate-errors'
+    ],
+    output: 'html'
+  };
+
+
+  const { report, lhr } = await launchChromeAndRunLighthouse(url, opts)
+
+  
+  console.log(`Lighthouse scores:\n${Object.keys(lhr.categories).map(key => {return `${key}:${lhr.categories[key]['score']}`}).join('\n')}`);
+
+  // await pWriteFile("out.html", report);
+
+  // await pWriteFile("output.json", JSON.stringify(lhr),'utf8', (err) => {
+  //     if(err !== null)
+  //       return console.error(err);
+  // })
+};
+
+module.exports = {
+  run
+}
