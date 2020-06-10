a2be0f5d11fb9dab14696d87e12f5391af4cedcd
Sijoon Lee <shijoonlee@gmail.com>
2020-06-10 10:25:28 -0700

    testing

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -2,13 +2,13 @@
 echo $INPUT_TARGET_ADDRESS
 cd /usr/src/app
 node command.js --addr $INPUT_TARGET_ADDRESS \
-                --exportTo html \
-                --willCheckPass no \
-                --minPerformance 0.9 \
-                --minAccessibility 0.9 \
-                --minBestPractices 0.9 \
-                --minSEO 0.9 \
-                --minPWA 0.9
+                --exportTo $INPUT_EXPORT_TO \
+                --willCheckPass $INPUT_WILL_CHECK_PASS \
+                --minPerformance $INPUT_MIN_PERFORMANCE \
+                --minAccessibility $INPUT_MIN_ACCESSIBILITY \
+                --minBestPractices $INPUT_MIN_BEST_PRACTICES \
+                --minSEO $INPUT_MIN_SEO \
+                --minPWA $INPUT_MIN_PWA
 
 cp ./report.html /github/workspace/report.html
 
